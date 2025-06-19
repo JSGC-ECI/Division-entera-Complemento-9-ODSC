50653f12bf10a8f957f5fad1fe2da6689fbb6fcf
Juan Sebastian Guayazan Clavijo <162055263+JuanSebastianGuayazanClavijoECI@users.noreply.github.com>
2025-06-19 08:10:48 -0500

    Update division.c

--- division.c
+++ division.c
@@ -2,3 +2,10 @@ bool mayor(int a, int b)
 {
     return a > b;
 }
+
+void convertirACifras(char numeroStr[], int cifras[], int *longitud) {
+    *longitud = strlen(numeroStr); // longitud del número
+    for (int i = 0; i < *longitud; i++) {
+        cifras[i] = numeroStr[i] - '0'; // convertir carácter a dígito
+    }
+}
