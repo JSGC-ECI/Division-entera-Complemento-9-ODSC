85b62774c609e2099ece91e5049a170c54571ec6
JSGC <162055263+JSGC-ECI@users.noreply.github.com>
2025-06-23 23:58:23 -0500

    Update division.c

--- division.c
+++ division.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
 
-void main() {
+int restar_complemento9(int minuendo, int sustraendo) {
+    int m[10];
+    int s[10];
+    int c9[10];
+    int suma[11];
+    
+    int tmin = 0, tsus = 0;
+
+    int temp = minuendo;
+    while (temp > 0) {
+        m[tmin++] = temp % 10;
+        temp /= 10;
+    }
+
+    temp = sustraendo;
+    while (temp > 0) {
+        s[tsus++] = temp % 10;
+        temp /= 10;
+    }
+
+    for (int i = 0; i < tmin; i++) {
+        if (i < tsus) c9[i] = 9 - s[i];
+        else c9[i] = 9;
+    }
+
+    int acarreo = 1;
+    for (int i = 0; i < tmin; i++) {
+        suma[i] = m[i] + c9[i] + acarreo;
+        acarreo = suma[i] / 10;
+        suma[i] %= 10;
+    }
+
+    int resultado = 0;
+    for (int i = tmin - 1; i >= 0; i--) {
+        resultado = resultado * 10 + suma[i];
+    }
+
+    return resultado;
+}
+
+int main() {
     int plin;
     scanf("%d", &plin);
 
@@ -9,11 +49,12 @@ void main() {
         int divisor;
         int cociente = 0;
         int residuo;
+
         scanf("%d", &dividendo);
-        scanf("%d", &divisor);;
+        scanf("%d", &divisor);
 
         while (dividendo >= divisor) {
-            dividendo -= divisor;
+            dividendo = restar_complemento9(dividendo, divisor);
             cociente++;
         }
 
@@ -22,4 +63,6 @@ void main() {
         printf("%d\n", cociente);
         printf("%d\n", residuo);
     }
+
+    return 0;
 }
